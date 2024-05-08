6035dc476529d45f8d7b05ae69bc1b272de0f606
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-05-08 08:58:21 +0000

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,19 @@ b=st.number_input("身長(m)")
 if st.button("計算"):
     c=a/b**2
     st.write(c)
+
+
+import random
+st.title("おみくじ")
+if st.button("おみくじ"):
+    omikuzi=random.randint(1,6)
+if omikuzi==1:
+    st.text("大吉")
+elif omikuzi==2:
+    st.text("狂")
+elif omikuzi==3:
+    st.text("吉")
+elif omikuzi==4:
+    st.text("小吉")
+else:
+    st.text("大凶")
