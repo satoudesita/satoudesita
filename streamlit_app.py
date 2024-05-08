c3952ffe733e521495e3430f7c3945c5e2858b28
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-05-08 08:17:55 +0000

    321dasd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,14 @@
 import streamlit as st
-import random
+st.title("bmi診断")
+st.html("""
+    <p style="color:red; font-size:30px;">元気になろう！！</p>
+    <img src="https://d3cmdai71kklhc.cloudfront.net/post_watermark/contest/104963/ct_20201108-200339480_pudwd.jpg">
+                """)
+st.text("体重を入力してください")
+a=st.number_input("体重")
+st.text("身長を入力してください")
+b=st.number_input("身長(m)")
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "引き分け"
-    elif (user_choice == "グー" and computer_choice == "チョキ") or \
-         (user_choice == "チョキ" and computer_choice == "パー") or \
-         (user_choice == "パー" and computer_choice == "グー"):
-        return "勝ち"
-    else:
-        return "負け"
-
-def main():
-    st.title("じゃんけんゲーム")
-    st.write("選んでください：")
-    
-    user_choice = st.radio("", ("グー", "チョキ", "パー"))
-    computer_choice = random.choice(["グー", "チョキ", "パー"])
-    
-    st.write(f"あなたの選択: {user_choice}")
-    st.write(f"コンピューターの選択: {computer_choice}")
-    
-    result = determine_winner(user_choice, computer_choice)
-    st.write(f"結果: {result}")
-
-if __name__ == "__main__":
-    main()
+if st.button("計算"):
+    c=a/b**2
+    st.write(c)
