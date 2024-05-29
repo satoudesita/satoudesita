adb417762e13967fa8d6e5d92b39be23eb6bf74c
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-05-29 08:58:49 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,49 @@
+
 import streamlit as st
-st.title("bmi診断")
-st.html("""
-    <p style="color:red; font-size:30px;">元気になろう！！</p>
-    <img src="https://d3cmdai71kklhc.cloudfront.net/post_watermark/contest/104963/ct_20201108-200339480_pudwd.jpg">
-                """)
-st.text("体重を入力してください")
-a=st.number_input("体重")
-st.text("身長を入力してください")
-b=st.number_input("身長(m)")
-
-if st.button("計算"):
-    c=a/b**2
-    st.write(c)
+import pandas as pd
+import numpy as np
 
 
-import random
-st.title("おみくじ")
-if st.button("おみくじ"):
-    omikuzi=random.randint(1,6)
-if omikuzi==1:
-    st.text("大吉")
-elif omikuzi==2:
-    st.text("狂")
-elif omikuzi==3:
-    st.text("吉")
-elif omikuzi==4:
-    st.text("小吉")
-else:
-    st.text("大凶")
+
+st.set_page_config(page_title="歴史問題ガチャ")
+
+# タイトルと説明
+st.title('歴史問題単語ガチャ')
+
+st.write('歴史の問題をランダムに表示して、勉強をサポートします！')
+st.write('がんばってください！')
+
+# Load the data
+@st.cache
+def load_data():
+    return pd.read_excel("zyouhou.xlsx")
+
+words_df = load_data()
+
+
+# ガチャ機能
+if st.button('ガチャを引く！'):
+    rarity_probs = {
+        'N': 0.4,
+        'R': 0.3,
+        'SR': 0.2,
+        'SSR': 0.1
+    }
+    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
+    subset_df = words_df[words_df['レア度'] == chosen_rarity]
+    selected_word = subset_df.sample().iloc[0]
+    
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
+
+if 'selected_word' in st.session_state:
+    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
+
+    # 意味を確認するボタンを追加
+    if st.button('意味を確認する'):
+        st.session_state.display_meaning = True
+
+    if st.session_state.display_meaning:
+        st.write(f"意味: {st.session_state.selected_word['意味']}")
